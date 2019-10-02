f56f34ac0333ce997625bd362c09b7482945ef6d
Aliencode-R <risharma654@gmail.com>
2019-10-02 19:51:00 +0530

    added gradient animation

--- MusicPlayer/app/src/main/java/com/example/musicplayer/MainActivity.java
+++ MusicPlayer/app/src/main/java/com/example/musicplayer/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.musicplayer;
 
+import android.graphics.drawable.AnimationDrawable;
 import android.media.MediaPlayer;
+import android.support.constraint.ConstraintLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.TextPaint;
@@ -15,6 +17,11 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         //final MediaPlayer mediaPlayer = MediaPlayer.create(this , R.raw.on_my_way);
+        ConstraintLayout constraintLayout = findViewById(R.id.layout);
+        AnimationDrawable animationDrawable = (AnimationDrawable) constraintLayout.getBackground();
+        animationDrawable.setEnterFadeDuration(2000);
+        animationDrawable.setExitFadeDuration(4000);
+        animationDrawable.start();
         mediaPlayer = MediaPlayer.create(MainActivity.this , R.raw.on_my_way);
         Button playbutton = findViewById(R.id.Play_button);
         playbutton.setOnClickListener(new View.OnClickListener() {
